64e4f64c1f75d9f98ea3d358d8a43f009dd184bf
Robbe Elsermans <robels@outlook.com>
2023-03-31 08:31:03 +0200

    probeersel bullets en werkt

--- SPaceInvader/src/main/java/be/uantwerpen/fti/ei/spaceinvaders/game/entitysystem/PlayerShootSystem.java
+++ SPaceInvader/src/main/java/be/uantwerpen/fti/ei/spaceinvaders/game/entitysystem/PlayerShootSystem.java
@@ -19,13 +19,22 @@ public class PlayerShootSystem {
         remainingIntervalTime += nextIntervalNS - System.nanoTime();
         remainingIntervalTime /= 1000;
 
-        if(input.inputAvailable() && input.isShoot() && remainingIntervalTime <=0){//Als er input aanwezig is & er is geschoten & (de timer is afgelopen of er is nog niet geschoten)
+        if(input.inputAvailable() && input.isShoot() && remainingIntervalTime <=0 && !hasFire){//Als er input aanwezig is & er is geschoten & (de timer is afgelopen of er is nog niet geschoten)
             nextIntervalNS +=intervalNs;
+            hasFire = true;
             return true;
+        }
+        else if(remainingIntervalTime <=0 && hasFire){
+            hasFire = false;
+            nextIntervalNS +=intervalNs;
+        }
+        else if(remainingIntervalTime <= 0){
+            nextIntervalNS +=intervalNs;
         }
 
         System.out.println("timer: " + String.valueOf(remainingIntervalTime/1000000) + "\t\t to: " + String.valueOf((nextIntervalNS - System.nanoTime())/1000000));
-
+        //TODO: fix dat er maar 1 bullet geschoten wordt
+        // Als er meerdere toets slagen achter elkaar binnen komen, mogen we maar om de 500 ms een toets doorlaten.
         return false;
     }
 
